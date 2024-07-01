7ab8c2ead7b690fd9db0f516542f2f6f3bb10b24
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2024-07-01 22:01:59 +0100

    time changed

--- scripts.js
+++ scripts.js
@@ -3,10 +3,11 @@ function updateTime() {
     const currentDay = document.querySelector('[data-testid="currentDay"]');
 
     const now = new Date();
+    const nowPlusOneHour = new Date(now.getTime() + 60 * 60 * 1000); // Add one hour (60 minutes * 60 seconds * 1000 milliseconds)
     const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const dayName = dayNames[now.getUTCDay()];
+    const dayName = dayNames[nowPlusOneHour.getUTCDay()];
 
-    currentTimeUTC.textContent = `Current Time (UTC): ${now.toUTCString()}`;
+    currentTimeUTC.textContent = `Current Time (UTC+1): ${nowPlusOneHour.toUTCString()}`;
     currentDay.textContent = `Current Day: ${dayName}`;
 }
 
